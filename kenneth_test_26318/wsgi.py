a9ae80d5e6c3885783bf59fb406ab50dcc26381b
Crowdbotics <team@crowdbotics.com>
2021-05-07 07:22:34 +0000

    Initial commit.

---
+++ kenneth_test_26318/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for kenneth_test_26318 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kenneth_test_26318.settings')
+
+application = get_wsgi_application()
